ce6b3ab3fd771cb7e9b059a6c9977bd29a000527
Carlos Mañas <carlos@spacenomads.com>
2021-01-05 13:43:24 +0100

    Add basic functionality

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,28 +1,49 @@
 console.log('🦄');
 
-const TESTEVENT = `BEGIN:VCALENDAR
-VERSION:2.0
-CALSCALE:GREGORIAN
-BEGIN:VEVENT
-SUMMARY:Access-A-Ride Pickup
-DTSTART;TZID=America/New_York:20130802T103400
-DTEND;TZID=America/New_York:20130802T110400
-LOCATION:1000 Broadway Ave.\, Brooklyn
-DESCRIPTION: Access-A-Ride trip to 900 Jay St.\, Brooklyn
-STATUS:CONFIRMED
-SEQUENCE:3
-BEGIN:VALARM
-TRIGGER:-PT10M
-DESCRIPTION:Pickup Reminder
-ACTION:DISPLAY
-END:VALARM
-END:VEVENT
-END:VCALENDAR`;
+//const NEWLINE = (navigator.appVersion.indexOf('Win') !== -1) ? '\r\n' : '\n';
+const NEWLINE = '\n';
 
-const ICSTESTFILE = 'data:text/calendar;charset=utf-8,' + escape(TESTEVENT);
+const YEAR = new Date().getFullYear();
+const title = 'Evento de prueba';
+const tzid = 'Europe/Madrid';
+const tStart = `${YEAR}0107T213000`;
+const tEnd = `${YEAR}0107T230000`;
+const vAlarm = '15';
 
-const container = document.querySelector('.js__ics-list');
+const calendarData = {title, tzid, tStart, tEnd, vAlarm };
 
-container.innerHTML =`
-<li class="app__download-item"><a href="${ICSTESTFILE}" download="evento.ics">Descarga el evento de prueba</a></li>
-`;
+function getCalendar(data) {
+	const {title, tzid, tStart, tEnd, vAlarm } =  data;
+
+	const eventTemplate = [
+		`BEGIN:VCALENDAR`,
+		`VERSION:2.0`,
+		`CALSCALE:GREGORIAN`,
+		`BEGIN:VEVENT`,
+		`SUMMARY:[Se(c)ta] "${title}"`,
+		`DTSTART;TZID=${tzid}:${tStart}`,
+		`DTEND;TZID=${tzid}:${tEnd}`,
+		`LOCATION:Online (Zoom)`,
+		`DESCRIPTION:[La Sombra] Club de lectura: Hoy toca "${title}"`,
+		`STATUS:CONFIRMED`,
+		`SEQUENCE:0`,
+		`BEGIN:VALARM`,
+		`TRIGGER:-PT${vAlarm}M`,
+		`DESCRIPTION:Prepárate para la Se(c)ta!`,
+		`ACTION:DISPLAY`,
+		`END:VALARM`,
+		`END:VEVENT`,
+		`END:VCALENDAR`,
+	];
+
+	return eventTemplate.join(NEWLINE);
+}
+
+const setaCalendar = encodeURIComponent(getCalendar(calendarData));
+const ICSTESTFILE = `data:text/calendar;charset=utf-8,${setaCalendar}` ;
+
+const icsBlock = document.querySelector('.js__ics');
+const icsButton = icsBlock.querySelector('.js__ics-button');
+
+icsButton.innerHTML =`<a href="${ICSTESTFILE}" download="calendario-seta.ics">Descarga el calendario de este mes</a>`;
+icsBlock.classList.add('app__calendar--visible');
