33271aec389e184212b570e41c86428ce44085b7
Carlos Mañas <carlos@spacenomads.com>
2021-01-08 21:11:39 +0100

    improve code

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,6 @@
-import {YEAR, CALENDARFILENAME, MONTHS, TSTART, TEND, ALARM} from './vars.js';
+import {YEAR, MONTHS, TSTART, TEND, ALARM} from './vars.js';
 import {getCalendar, getCalendars, getMonth, getMonthName, getCalendarUri} from './calendar.js';
-import {getEvents} from './event.js';
+import {getEvents, createFullLink, createEvents} from './event.js';
 import {isAndroid} from './android.js';
 
 const app = document.querySelector('.app');
@@ -16,48 +16,7 @@ const icsMonth = app.querySelector('.js__ics-month');
 
 
 
-function createFullLink(el, uri, label) {
-	el.innerHTML = '';
-	const downloadLink = document.createElement('a');
-	const downloadLinkText = document.createTextNode(label);
-	downloadLink.href = uri;
-	downloadLink.appendChild(downloadLinkText);
-	downloadLink.setAttribute('download', `${CALENDARFILENAME}.ics`);
-	el.appendChild(downloadLink);
-}
-
-
-
-
-function createItem(el, data) {
-	const {calendarUri, dow, day, title} =  data;
-	const item = `<li class="app__calendar-event">
-		<a href="${calendarUri}" class="app__calendar-event-link" download="${CALENDARFILENAME}-${day}.ics">
-			<span class="app__calendar-event-date">${dow} ${day}</span> <span class="app__calendar-event-title">${title}</span>
-		</a>
-	</li>`;
-
-	el.insertAdjacentHTML('beforeend', item);
-}
-
-
-
-
-
-function createEvents(el, calendar) {
-	el.innerHTML = '';
-	for (let i = 0; i < calendar.length; i++) {
-		const event = calendar[i];
-		createItem(el, event);
-	}
-}
-
-
-
-
-
 function showCalendarBlock(calendar, uri, month) {
-
 
 	createFullLink(icsFullCalendar, uri, 'Descargar calendario completo');
 	createEvents(icsEvents, calendar);
@@ -81,12 +40,11 @@ function showCalendarCode(data) {
 function createCalendar() {
 	const vAlarm = ALARM;
 	const message = telegramInput.value;
-
 	const month = getMonth(message, MONTHS);
+
 	const calendarData = getEvents(message).map(event => {
 		const {day, title} = event;
 		const strDay = day > 9 ? day : '0' + day;
-
 		const tStart = `${YEAR}${month}${strDay}${TSTART}`;
 		const tEnd = `${YEAR}${month}${strDay}${TEND}`;
 
@@ -95,7 +53,6 @@ function createCalendar() {
 
 	const setaFullCalendar = getCalendar(calendarData);
 	const setaCalendars = getCalendars(calendarData);
-	console.log(setaCalendars);
 	const icsFullSetaCalendar = getCalendarUri(setaFullCalendar) ;
 
 	showCalendarBlock(setaCalendars, icsFullSetaCalendar, month);
--- assets/js/event.js
+++ assets/js/event.js
@@ -1,4 +1,4 @@
-import {SENTENCE} from './vars.js';
+import {SENTENCE, CALENDARFILENAME} from './vars.js';
 
 function getDay(str) {
 	let messageDay = parseInt(str.trim().split(' ')[0]) || 1;
@@ -31,4 +31,44 @@ function getEvents(str) {
 
 
 
-export {getDay, getTitle, getEvents};
+function createFullLink(el, uri, label) {
+	el.innerHTML = '';
+	const downloadLink = document.createElement('a');
+	const downloadLinkText = document.createTextNode(label);
+	downloadLink.href = uri;
+	downloadLink.appendChild(downloadLinkText);
+	downloadLink.setAttribute('download', `${CALENDARFILENAME}.ics`);
+	el.appendChild(downloadLink);
+}
+
+
+
+
+function createItem(el, data) {
+	const {calendarUri, dow, day, title} =  data;
+	const item = `<li class="app__calendar-event">
+		<a href="${calendarUri}" class="app__calendar-event-link" download="${CALENDARFILENAME}-${day}.ics">
+			<span class="app__calendar-event-date">${dow} ${day}</span> <span class="app__calendar-event-title">${title}</span>
+		</a>
+	</li>`;
+
+	el.insertAdjacentHTML('beforeend', item);
+}
+
+
+
+
+
+function createEvents(el, calendar) {
+	el.innerHTML = '';
+	for (let i = 0; i < calendar.length; i++) {
+		const event = calendar[i];
+		createItem(el, event);
+	}
+}
+
+
+
+
+
+export {getDay, getTitle, getEvents, createFullLink, createItem, createEvents};
